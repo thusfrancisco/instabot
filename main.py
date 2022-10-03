5101a139b9ad91e0e0fa3f11aa59a910602a5912
that1francisco <franciscoabsampaio@protonmail.com>
2022-10-03 18:05:17 +0100

    Update .gitignore Add tests to login to Instagram Add .env functionality to pytest

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
 import time
 import re
 from playwright.sync_api import Page, expect
+import pytest
+import os
 
 
-def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
-    # chromium.launch(headless=False, slow_mo=100)
+@pytest.fixture()
+def username() -> str:
+    return os.environ.get("INSTA_USERNAME")
 
+
+@pytest.fixture()
+def password() -> str:
+    return os.environ.get("INSTA_PASSWORD")
+
+
+def test_goto_instagram_homepage_and_check_title(page: Page):
     page.goto("https://www.instagram.com/")
 
-    # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Instagram"))
+    
 
-    # create a locator
-    get_started = page.locator("text=Get Started")
-
-    # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # Click the get started link.
-    get_started.click()
-
-    # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
+def test_username_and_password_are_strings(username, password):
+    assert isinstance(username, str)
+    assert isinstance(password, str)
 
 
-class BotInstagram():
+def test_goto_instagram_page_and_login(page: Page, username, password):
+    page.goto("https://www.instagram.com/")
 
-    def __init__(self):
-        self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    button_only_allow_essential_cookies = page.locator('button:text("Only allow essential cookies")')
+    button_only_allow_essential_cookies.click()
 
-    def entrar_link(self, link):
-        self.driver.get(link)
+    input_username = page.locator('input[name="username"]')
+    input_username.type(username, delay=100)
 
-    def pegar_link_das_fotos(self):
-        os_links = self.driver.find_elements_by_tag_name('a')
-        
-        todos_os_links = []
-        for os_link in os_links:
-            href = os_link.get_attribute("href")
-            if(href.startswith("https://www.instagram.com/p/")):
-                todos_os_links.append(href)
-        
-        return todos_os_links
+    input_password = page.type('input[name="password"]', password, delay=100)
 
-    def dar_like(self):
-        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button').click()
+    button_log_in = page.locator('button>div:text("Log in")')
+    button_log_in.click()
 
-    def comentar(self, comentario):
-        textarea = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/textarea')
-        time.sleep(1)
-        textarea.click()
-        time.sleep(1)
-        textarea = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/textarea')
-        time.sleep(1)
-        textarea.clear()
-        time.sleep(1)
-        textarea.send_keys(comentario)
-        time.sleep(2)
-        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/button[2]').click()
+    button_dont_save_loggin_info = page.locator('button:text("Not Now")')
+    button_dont_save_loggin_info.click()
+
+    button_dont_turn_on_notifications = page.locator('button:text("Not Now")')
+    button_dont_turn_on_notifications.click()
+    
+    time.sleep(30)
